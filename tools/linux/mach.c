c6af44bd936afc169fa45b6ad8ea1ea2f73d2c2e
Max Nurzia <7797957+mnurzia@users.noreply.github.com>
2024-01-18 00:33:06 -0500

    mach: orthogonalize everything

--- tools/linux/mach.c
+++ tools/linux/mach.c
@@ -11,19 +11,20 @@
 #define MACH_RAM_SIZE (1024UL * 1024UL * 128UL) /* 128MiB of ram */
 #define MACH_DTB_OFFSET 0x2000000UL             /* dtb is @32MiB */
 
-#define MACH_PLIC_BASE 0xC000000UL  /* plic0 base address */
-#define MACH_CLINT_BASE 0x2000000UL /* clint0 base address */
-#define MACH_UART_BASE 0x3000000UL  /* uart0 base address */
-#define MACH_UART2_BASE 0x6000000UL /* uart1 base address */
+#define MACH_PLIC0_BASE 0xC000000UL  /* plic0 base address */
+#define MACH_CLINT0_BASE 0x2000000UL /* clint0 base address */
+#define MACH_UART0_BASE 0x3000000UL  /* uart0 base address */
+#define MACH_UART1_BASE 0x6000000UL  /* uart1 base address */
 
 typedef struct mach {
   rv *cpu;
   rv_u8 *ram;
-  rv_plic plic;
-  rv_clint clint;
-  rv_uart uart, uart2;
+  rv_plic plic0;
+  rv_clint clint0;
+  rv_uart uart0, uart1;
 } mach;
 
+/* machine general bus access */
 rv_res mach_bus(void *user, rv_u32 addr, rv_u8 *data, rv_u32 store,
                 rv_u32 width) {
   mach *m = (mach *)user;
@@ -31,21 +32,23 @@ rv_res mach_bus(void *user, rv_u32 addr, rv_u8 *data, rv_u32 store,
     rv_u8 *ram = m->ram + addr - MACH_RAM_BASE;
     memcpy(store ? ram : data, store ? data : ram, width);
     return RV_OK;
-  } else if (addr >= MACH_PLIC_BASE && addr < MACH_PLIC_BASE + RV_PLIC_SIZE) {
-    return rv_plic_bus(&m->plic, addr - MACH_PLIC_BASE, data, store, width);
-  } else if (addr >= MACH_CLINT_BASE &&
-             addr < MACH_CLINT_BASE + RV_CLINT_SIZE) {
-    return rv_clint_bus(&m->clint, addr - MACH_CLINT_BASE, data, store, width);
-  } else if (addr >= MACH_UART_BASE && addr < MACH_UART_BASE + RV_UART_SIZE) {
-    return rv_uart_bus(&m->uart, addr - MACH_UART_BASE, data, store, width);
-  } else if (addr >= MACH_UART2_BASE && addr < MACH_UART2_BASE + RV_UART_SIZE) {
-    return rv_uart_bus(&m->uart2, addr - MACH_UART2_BASE, data, store, width);
+  } else if (addr >= MACH_PLIC0_BASE && addr < MACH_PLIC0_BASE + RV_PLIC_SIZE) {
+    return rv_plic_bus(&m->plic0, addr - MACH_PLIC0_BASE, data, store, width);
+  } else if (addr >= MACH_CLINT0_BASE &&
+             addr < MACH_CLINT0_BASE + RV_CLINT_SIZE) {
+    return rv_clint_bus(&m->clint0, addr - MACH_CLINT0_BASE, data, store,
+                        width);
+  } else if (addr >= MACH_UART0_BASE && addr < MACH_UART0_BASE + RV_UART_SIZE) {
+    return rv_uart_bus(&m->uart0, addr - MACH_UART0_BASE, data, store, width);
+  } else if (addr >= MACH_UART1_BASE && addr < MACH_UART1_BASE + RV_UART_SIZE) {
+    return rv_uart_bus(&m->uart1, addr - MACH_UART1_BASE, data, store, width);
   } else {
     return RV_BAD;
   }
 }
 
-rv_res uart_io(void *user, rv_u8 *byte, rv_u32 write) {
+/* uart0 I/O callback */
+rv_res uart0_io(void *user, rv_u8 *byte, rv_u32 write) {
   int ch;
   static int thrott = 0; /* prevent getch() from being called too much */
   (void)user;
@@ -58,12 +61,14 @@ rv_res uart_io(void *user, rv_u8 *byte, rv_u32 write) {
   return RV_OK;
 }
 
-rv_res uart2_io(void *user, rv_u8 *byte, rv_u32 write) {
+/* uart1 I/O callback */
+rv_res uart1_io(void *user, rv_u8 *byte, rv_u32 write) {
   (void)user, (void)byte, (void)write;
   /* your very own uart, do whatever you want with it! */
   return RV_BAD; /* stubbed for now */
 }
 
+/* dumb bootrom */
 void load(const char *path, rv_u8 *buf, rv_u32 max_size) {
   FILE *f = fopen(path, "rb");
   if (!f) {
@@ -92,10 +97,10 @@ int main(int argc, const char *const *argv) {
 
   /* peripheral setup */
   rv_init(&cpu, &mach, &mach_bus);
-  rv_plic_init(&mach.plic);
-  rv_clint_init(&mach.clint, &cpu);
-  rv_uart_init(&mach.uart, NULL, &uart_io);
-  rv_uart_init(&mach.uart2, &mach, &uart2_io);
+  rv_plic_init(&mach.plic0);
+  rv_clint_init(&mach.clint0, &cpu);
+  rv_uart_init(&mach.uart0, NULL, &uart0_io);
+  rv_uart_init(&mach.uart1, &mach, &uart1_io);
 
   /* load kernel and dtb */
   load(argv[1], mach.ram, MACH_RAM_SIZE);
@@ -117,13 +122,13 @@ int main(int argc, const char *const *argv) {
       if (!++cpu.csr.mtime)
         cpu.csr.mtimeh++;
     rv_step(&cpu);
-    if (rv_uart_update(&mach.uart))
-      rv_plic_irq(&mach.plic, 1);
-    if (rv_uart_update(&mach.uart2))
-      rv_plic_irq(&mach.plic, 2);
-    irq = RV_CSI * rv_clint_msi(&mach.clint, 0) |
-          RV_CTI * rv_clint_mti(&mach.clint, 0) |
-          RV_CEI * rv_plic_mei(&mach.plic, 0);
+    if (rv_uart_update(&mach.uart0))
+      rv_plic_irq(&mach.plic0, 1);
+    if (rv_uart_update(&mach.uart1))
+      rv_plic_irq(&mach.plic0, 2);
+    irq = RV_CSI * rv_clint_msi(&mach.clint0, 0) |
+          RV_CTI * rv_clint_mti(&mach.clint0, 0) |
+          RV_CEI * rv_plic_mei(&mach.plic0, 0);
     rv_irq(&cpu, irq);
   } while (1);
   return EXIT_SUCCESS; /* challenge: try and make this program return 0 */
